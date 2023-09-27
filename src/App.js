b6617f843c93f5003e425c323ca7d05015dd1d8b
Siddharth Sharma S <145635834+siddharthss19959977@users.noreply.github.com>
2023-09-27 09:37:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+// The above is a react functional component
+
+export default App;
+
+// import React from 'react'
+
+// export const App = () => {
+//   return (
+//     <div>App</div>
+//   )
+// }
+// The above code is a react functional component using arrow function
+
+
+// import React, { Component } from 'react'
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>App</div>
+//     )
+//   }
+// }
+// The above code is a class component
+
+
+// Shortcut keys
+// rfc for react functional component
+// rafc for react functional component using arrow function
+// rcc for react class component
+
+// Use any as per requirement
